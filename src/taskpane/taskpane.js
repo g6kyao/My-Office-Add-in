bf1547294087198c26ec4528b90de610faec0861
jyao <wei321.yao@gmail.com>
2023-05-24 14:06:48 +0200

    format

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -8,54 +8,52 @@
 Office.onReady((info) => {
   if (info.host === Office.HostType.Word) {
     // Assign event handlers and other initialization logic.
-document.getElementById("insert-paragraph").onclick = () => tryCatch(insertParagraph);
-document.getElementById("apply-style").onclick = () => tryCatch(applyStyle);
-document.getElementById("apply-custom-style").onclick = () => tryCatch(applyCustomStyle);
+    document.getElementById("insert-paragraph").onclick = () => tryCatch(insertParagraph);
+    document.getElementById("apply-style").onclick = () => tryCatch(applyStyle);
+    document.getElementById("apply-custom-style").onclick = () => tryCatch(applyCustomStyle);
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
-
   }
 });
 
 async function insertParagraph() {
   await Word.run(async (context) => {
-
-      // TODO1: Queue commands to insert a paragraph into the document.
-      const docBody = context.document.body;
-      docBody.insertParagraph("Office has several versions, including Office 2016, Microsoft 365 subscription, and Office on the web.",
-                              Word.InsertLocation.start);
-      await context.sync();
+    // TODO1: Queue commands to insert a paragraph into the document.
+    const docBody = context.document.body;
+    docBody.insertParagraph(
+      "Office has several versions, including Office 2016, Microsoft 365 subscription, and Office on the web.",
+      Word.InsertLocation.start
+    );
+    await context.sync();
   });
 }
 
 /** Default helper for invoking an action and handling errors. */
 async function tryCatch(callback) {
   try {
-      await callback();
+    await callback();
   } catch (error) {
-      // Note: In a production add-in, you'd want to notify the user through your add-in's UI.
-      console.error(error);
+    // Note: In a production add-in, you'd want to notify the user through your add-in's UI.
+    console.error(error);
   }
 }
 
 async function applyStyle() {
   await Word.run(async (context) => {
+    // TODO1: Queue commands to style text.
+    const firstParagraph = context.document.body.paragraphs.getFirst();
+    firstParagraph.styleBuiltIn = Word.Style.intenseReference;
 
-      // TODO1: Queue commands to style text.
-      const firstParagraph = context.document.body.paragraphs.getFirst();
-firstParagraph.styleBuiltIn = Word.Style.intenseReference;
-
-      await context.sync();
+    await context.sync();
   });
 }
 
 async function applyCustomStyle() {
   await Word.run(async (context) => {
+    // TODO1: Queue commands to apply the custom style.
+    const lastParagraph = context.document.body.paragraphs.getLast();
+    lastParagraph.style = "MyCustomStyle";
 
-      // TODO1: Queue commands to apply the custom style.
-      const lastParagraph = context.document.body.paragraphs.getLast();
-lastParagraph.style = "MyCustomStyle";
-
-      await context.sync();
+    await context.sync();
   });
 }
